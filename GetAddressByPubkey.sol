555360fbb3068ac06634adc0990c39c3bc919d20
Hagile <1182696093@qq.com>
2018-04-04 11:04:21 +0800

    Create GetAddressByPubkey.sol

---
+++ GetAddressByPubkey.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.4;
+contract GetAddressByPubKey {
+    bytes tmp = new bytes(64);
+    
+    function getAddress(bytes publicKey)public returns(address){
+        uint i=0;
+        
+        uint len = publicKey.length;
+        if (publicKey.length > 64){
+            for(i=len - 64;i<len;i++){
+                tmp[i-1] = publicKey[i];
+            }
+        }else{
+            
+            for(i=0; i<len; i++){
+                tmp[i] = publicKey[i];
+            }
+            
+            for(i=tmp.length-1; i>=len; i--){
+                tmp[i] = 0;
+            }
+        }
+        return address(keccak256(tmp));
+    }
+}
